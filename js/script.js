779782bd83af6c77f741ab8d99a97cc1fb31b216
Łukasz Nowak <enowuigrek@gmail.com>
2022-02-28 22:16:59 +0100

    done module 5.4

--- js/script.js
+++ js/script.js
@@ -38,3 +38,54 @@ const links = document.querySelectorAll('.titles a');
 for(let link of links){
   link.addEventListener('click', titleClickHandler);
 }
+
+const optArticleSelector = '.post',
+  optTitleSelector = '.post-title',
+  optTitleListSelector = '.titles';
+
+function generateTitleLinks(){
+
+    /* remove contents of titleList */
+    const titleList = document.querySelector(optTitleListSelector);
+    titleList.innerHTML = '';
+    console.log(titleList)
+
+    /* find all the articles and save them to variable: articles */
+    /* ... */
+
+    let html = '';
+
+    /* for each article */
+    const articles = document.querySelectorAll(optArticleSelector);
+
+    for(let article of articles) {
+
+        /* get the article id */
+        const articleId = article.getAttribute('id');
+        /* find the title element */
+        
+        /* get the title from the title element */
+        const articleTitle = article.querySelector(optTitleSelector).innerHTML;
+        console.log(articleTitle);
+
+
+        /* create HTML of the link */
+        const linkHTML = '<li><a href="#' + articleId + '"><span>' + articleTitle + '</span></a></li>';
+        console.log('linkHTML', linkHTML);
+
+        /* insert link into titleList */
+
+        /* insert link into html variable */
+        html = html + linkHTML;
+    }
+    titleList.innerHTML = html;
+    const links = document.querySelectorAll('.titles a');
+    console.log(links);
+
+    for(let link of links){
+    link.addEventListener('click', titleClickHandler);
+    }
+    
+
+}
+generateTitleLinks();
